54f89614892a7227c44166bbf83c978ad37448dc
Lilo <larafabianribeiro@icloud.com>
2024-09-05 21:47:12 -0300

    Add files via upload

---
+++ AlunosAPI/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();  
+
+var app = builder.Build();
+//Swagger Prte 2
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json","Crud Pessoa v1");
+        c.RoutePrefix= string.Empty;
+    });
+}
+
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ AlunosAPI/Repositories/PessoaRepository.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace AlunosAPI.Repositories
+{
+    public class PessoaRepository
+    {
+        private readonly string _connectionString;
+       
+        public PessoaRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        private IDbConnection Connection =>
+            new MySqlConnection(_connectionString);
+    }
+}
